f0fb375d61bba6ae1c8b7aa8ad3ee22c45a9af50
Woomunsik <wms170513@gmail.com>
2023-06-04 14:24:50 +0900

    상수 추가

--- lib/CH1/variable.dart
+++ lib/CH1/variable.dart
@@ -1,5 +1,7 @@
 class Variable {
 
+  ///변수 : String, int, double, bool, var, dynamic
+  ///변수 = 값을 변경할 수 있는 수
   Variable(){
 
     String name = "우문식";
@@ -13,5 +15,21 @@ class Variable {
     age = 50;
     print(age);
 
+    // variable
+    // var 타입은 최초 입력한 타입을 인식
+    var b = 10;
+
+    // var 타입과 비슷
+    // 모든 타입을 입력할 수 있다.
+    // 실제 개발할 떄 이게 무슨 값인지 알기 어려움
+    dynamic d = "우문식"
+    d = 12;
+    d = 22.3;
+    d = false;
+
+    //상수
+    final String address = "seuol";
+    const q = "ff";
+    // address = 'busan';
   }
 }
