b75411e208d9a86a219b3a0c0fcdad106ea01331
Woomunsik <wms170513@gmail.com>
2023-06-04 14:12:08 +0900

    변수 추가

---
+++ lib/CH1/variable.dart
@@ -0,0 +1,17 @@
+class Variable {
+
+  Variable(){
+
+    String name = "우문식";
+    print(name);
+    double weight = 83.2;
+    bool check = false;
+    print(check);
+
+    int age = 34;
+    print(age);
+    age = 50;
+    print(age);
+
+  }
+}
