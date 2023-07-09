668c8eb0006d8a2b3cd8e3496171c000deaf4187
Woomunsik <wms170513@gmail.com>
2023-07-09 10:06:47 +0900

    MediaQuery

--- lib/ch5/pageview_screen.dart
+++ lib/ch5/pageview_screen.dart
@@ -8,21 +8,59 @@ class PageViewScreen extends StatefulWidget {
 }
 
 class _PageViewScreenState extends State<PageViewScreen> {
+  final PageController pageController = PageController();
+
+  double opacity = 1;
+
+  @override
+  void initState() {
+    pageController.addListener(() {
+      if (pageController.page! < 1) {
+        setState(() {
+          opacity = pageController.page!;
+        });
+      }
+      print(pageController.page);
+
+      /// addListener : PageView가 동작할때마다 호출
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('PageViewScreen'),
       ),
-      body: PageView(
-        onPageChanged: (value) {
-          print('value : $value');
-        },
-        scrollDirection: Axis.vertical,
+      body: Column(
         children: [
-          Container(color: Colors.red),
-          Container(color: Colors.green),
-          Container(color: Colors.brown),
+          Opacity(
+            opacity: opacity,
+            child: Container(
+              color: Colors.black,
+              height: 50,
+            ),
+          ),
+          Expanded(
+            child: PageView(
+              controller: pageController,
+              onPageChanged: (value) {
+                print('value : $value');
+              },
+              children: [
+                Container(color: Colors.red),
+                Container(color: Colors.green),
+                Container(color: Colors.brown),
+              ],
+            ),
+          ),
         ],
       ),
     );
